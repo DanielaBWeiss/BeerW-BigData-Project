09b6aa3445982cf588817ac870f1d62209664018
Ron Sinai <ron564@gmail.com>
2019-09-26 00:46:37 +0300

    Improve GC bin 3-5 heuristics

--- heuristics/bin_3to5.py
+++ heuristics/bin_3to5.py
@@ -8,6 +8,14 @@ SOCIAL_GATHERING = "SOCIAL-GATHERING"
 AFTER_WORK = "AFTER-WORK"
 UNK = "UNKNOWN"
 
+
+'''
+Improvements suggestions:
+1. Use a better kids_meal feature
+2. Use a main_dish feature (& total_main_dishes)
+'''
+
+
 class Bin3to5Classifier:
     def __init__(self):
         pass
@@ -17,7 +25,7 @@ class Bin3to5Classifier:
             return BREAKFAST
         elif hour >= 11 and hour <= 14:
             return LUNCH
-        elif hour >= 18 and hour < 22:
+        elif hour >= 16 and hour < 22:
             return DINNER
         
         return UNK
@@ -32,22 +40,22 @@ class Bin3to5Classifier:
     
     def classify(self, table):
         # if table contains "K" as for kids - return FAMILY_EVENT
-        if table["kids_meal"]:
+        if table["kids_meal"].iloc[0]:
             return FAMILY_EVENT
 
         # else if table contains kids-related items (pancakes, milkshake etc.) - return FAMILY_EVENT
         # TODO: create a list of kids-related items
 
         # else if table contains mostly alcoholic drinks w/o main dishes - return DRINKING
-        if table["total_orders_category_id_3.0"] > 0 and table["total_orders_category_id_2.0"] == 0:
+        if table["total_orders_category_id_3.0"].iloc[0] > 0 and table["total_orders_category_id_2.0"].iloc[0] == 0:
             return DRINKING
         # TODO: figure out WHAT ARE main dishes? (again, create a list / take the whole category_id 2)
 
         # else if table contains shareable dishes w/ drinks, return:
         # - AFTER_WORK if occurs around after-work hours (15-18) during the weekdays (Monday to Friday)
         # - SOCIAL_GATHERING anytime else
-        if table["sharable"]:
-            if self._is_after_work(table["order_day_of_week"], table["order_hour"]):
+        if table["sharable"].iloc[0] and table["total_orders_category_id_2.0"].iloc[0] < table["guest_count"].iloc[0]:
+            if self._is_after_work(table["order_day_of_week"].iloc[0], table["order_hour"].iloc[0]):
                 return AFTER_WORK
             return SOCIAL_GATHERING
         
@@ -57,7 +65,7 @@ class Bin3to5Classifier:
         # - LUNCH
         # - DINNER
         # TODO: use the pre-defined ToD table / generate a new one
-        if table["total_orders_category_id_2.0"] >= table["total_orders"] / 2:
-            return self._ToD(table.order_hour)
+        if table["total_orders_category_id_2.0"].iloc[0] >= (table["guest_count"].iloc[0] / 2):
+            return self._ToD(table["order_hour"].iloc[0])
         
         return UNK
